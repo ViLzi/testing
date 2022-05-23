7bcb19b6abefe15b53d3b0214dd372d865cf4ee1
Simeon <sssreddevilsss@abv.bg>
2022-05-23 21:27:14 +0300

    updated
    shit

--- testModule/src/test.java
+++ testModule/src/test.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
-        
+
+
+        Scanner sc = new Scanner (System.in);
+        String day = sc.nextLine();
+        int age = Integer.parseInt(sc.nextLine());
+        int price = 0;
+        boolean isNotValid = true;
+        if (age >= 0 && age <= 18){
+
+            if (day.equals("Weekday")){
+                price = 12;
+            } else if (day.equals("Weekend")){
+                price = 15;
+            } else if (day.equals("Holiday")){
+                price = 5;
+            }
+
+        } else if (age > 18 && age <= 64){
+            if (day.equals("Weekday")){
+                price = 18;
+            } else if (day.equals("Weekend")){
+                price = 20;
+            } else if (day.equals("Holiday")){
+                price = 12;
+            }
+
+        } else if (age > 64 && age <= 122){
+            if (day.equals("Weekday")){
+                price = 12;
+            } else if (day.equals("Weekend")){
+                price = 15;
+            } else if (day.equals("Holiday")){
+                price = 10;
+            }
+
+        }
+        if (isNotValid) {
+            System.out.println("Error!");
+        } else {
+            System.out.printf("%d$", price);
+        }
     }
 }
